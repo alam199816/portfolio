070dcff346b3242b776e5a88352bcb703791f8cb
Khursheed Alam <khurshidalam199816@gmail.com>
2024-05-21 03:46:44 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,14 +1,10 @@
-// db.js
 import mysql from 'mysql';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const con = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'portfolio_db'
+    host: 'localhost',
+    user: 'root',
+    password: '', 
+    database: 'portfolio_db'
 });
 
 con.connect(err => {
